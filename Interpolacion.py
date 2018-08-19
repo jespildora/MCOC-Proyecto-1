af528bf797841877f2b3ddf77fb6101c02daa1d3
Benjamín Castro <41970396+castropohl@users.noreply.github.com>
2018-08-19 15:18:34 -0300

    Update Interpolacion.py

--- Interpolacion.py
+++ Interpolacion.py
@@ -2,8 +2,8 @@ import scipy as sp
 from scipy import interpolate
 import matplotlib.pyplot as plt
 
-datos = sp.loadtxt("20161009-010221-C18O-HNE.txt")
-x = sp.arange(len(datos))
+datos = sp.loadtxt("20161009-010221-C18O-HNE.txt") #Arreglo con registros sismicos
+x = sp.arange(len(datos)) #Arreglo para construir eje x segun cantidad de registros
 f = interpolate.interp1d(x, datos)
 xnew = sp.arange(0, len(datos), 1)
 ynew = f(xnew)
